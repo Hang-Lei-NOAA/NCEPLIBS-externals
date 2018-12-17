a08aab04bb7d56607f281cf54719127801029833
hang.lei <Jun.Wang@noaa.gov>
2018-12-17 14:47:05 -0500

    esmf_v7.1.0r

--- build.sh
+++ build.sh
@@ -3,48 +3,84 @@ set -eux
 #-----------------------------------------------------
 cwd=`pwd`
 source ../../../machine-setup.sh > /dev/null 2>&1
+module use ../../modulefiles
 
-export LIBNAME=crtm
-export VER=v2.2.3
-export FCMP=ifort
-export CCMP=icc
-export LIB_TEMP=${LIBNAME}_${VER}
+export LIBNAME=esmf
+export VER=v7.1.0r
+export FC=ifort
+export cc=icc
 
 if [ $target = wcoss_cray ]; then
-  export FCMP=ftn
+  export ESMF_COMM=mpi
+  export ESMF_OS=Unicos
   module load PrgEnv-intel
   module load craype-sandybridge
+  module load netcdf/v4.4.4.1
+  module load hdf5/v1.8.18
+  module load xt-lsfhpc/9.1.3
 elif [ $target = "wcoss" ]; then
   module load ics/12.1
+  module load netcdf/v4.4.4.1
+  module load hdf5/v1.8.18
+  export ESMF_OS=Linux
+  export ESMF_COMM=intelmpi
 elif [ $target = "theia" ]; then
   module load intel/14.0.2
+  module load impi/5.1.2.150
+  module load netcdf/v4.4.4.1
+  module load hdf5/v1.8.18
+  export ESMF_OS=Linux
+  export ESMF_COMM=intelmpi
 elif [ $target = "gaea" ]; then
-  :
+  export ESMF_COMM=mpi
+  export ESMF_OS=Unicos
+  module load xt-lsfhpc/9.1.3
+  module load netcdf/v4.4.4.1
+  module load hdf5/v1.8.18
 elif [ $target = "jet" ]; then
   module load intel/12.1.4
+  module load mvapich2/1.8
+  module load netcdf/v4.4.4.1
+  module load hdf5/v1.8.18
+  export ESMF_OS=Linux
+  export ESMF_COMM=mpich2
 fi
 
 ###########################################################
-# Repository site:
-# svn co https://svnemc.ncep.noaa.gov/projects/crtm/releases/REL-2.2.3
-###########################################################
-tar xf REL-2.2.3.tar.gz
-cd REL-2.2.3
-###########################################################
-rm -rf ../../../incmod/$LIB_TEMP ../../../libs/${LIBNAME}_${VER}/lib${LIB_TEMP}.a
-. config-setup/ifort.setup
-./configure --prefix=${PWD}
-make
-make install
-###########################################################
- mkdir -p ../../../libs/${LIBNAME}_${VER}
- mv $LIB_TEMP/lib/libcrtm.a ../../../libs/${LIBNAME}_${VER}/lib${LIB_TEMP}.a
- mkdir -p ../../../incmod/$LIB_TEMP
- mv $LIB_TEMP/include/*.mod ../../../incmod/$LIB_TEMP
-###########################################################
+cd ../../
+lwd=`pwd`
 cd $cwd
-rm -rf REL-2.2.3
+LOCAL_EXTERN=${lwd}/ext_libs/${LIBNAME}_${VER}
+mkdir -p $LOCAL_EXTERN
+rm -rf $LOCAL_EXTERN/*
 ###########################################################
+tar xf esmf_7_1_0r_src.tar.gz
+cd esmf
+#####
+cp ../common.mk ./build/.
+#####
+###########################################################
+export ESMF_INSTALL_PREFIX=$LOCAL_EXTERN
+export ESMF_DIR=`pwd`
+export ESMF_COMPILER=intel
+export ESMF_SITE=default
+export ESMF_INSTALL_MODDIR=mod
+export ESMF_INSTALL_BINDIR=bin
+export ESMF_INSTALL_LIBDIR=lib
+export ESMF_NETCDF=split
+export NETCDF_DIR=$NETCDF
+export ESMF_NETCDF_INCLUDE=$NETCDF_DIR/include
+export ESMF_NETCDF_LIBPATH=$NETCDF_DIR/lib
+export ESMF_F90COMPILEOPTS="-fp-model source"
+export ESMF_CXXCOMPILEOPTS="-fp-model source"
+###########################################################
+gmake clean
+gmake -j4 lib
+gmake install
+cd $cwd
+rm -rf esmf
+###########################################################
+
 #
 #     Create modulefile
 #
