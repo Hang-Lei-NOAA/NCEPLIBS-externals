5d5a80a4485578c69bc543b8a5e6090e7f047994
hang.lei <Jun.Wang@noaa.gov>
2018-12-17 14:58:40 -0500

    netcdf_v4.4.4

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -eux
 cwd=`pwd`
 source ../../../machine-setup.sh > /dev/null 2>&1
 module use ../../modulefiles
-module load hdf5/v1.8.18
 
 export LIBNAME=netcdf
 export VER=v4.4.4.1
@@ -15,44 +14,46 @@ if [ $target = wcoss_cray ]; then
   export FCMP=ftn
   module load PrgEnv-intel
   module load craype-sandybridge
+  module load hdf5/v1.8.18
+  module load netcdf/v4.4.4.1
 elif [ $target = "wcoss" ]; then
   module load ics/12.1
+  module load hdf5/v1.8.18
+  module load netcdf/v4.4.4.1
 elif [ $target = "theia" ]; then
   module load intel/14.0.2
+  module load hdf5/v1.8.18
+  module load netcdf/v4.4.4.1
 elif [ $target = "gaea" ]; then
-  :
+  module load hdf5/v1.8.18
+  module load netcdf/v4.4.4.1
 elif [ $target = "jet" ]; then
   module load intel/12.1.4
+  module load hdf5/v1.8.18
+  module load netcdf/v4.4.4.1
 fi
 
-export CPPFLAGS=$HDF5_INCLUDE_OPTS
-export LDFLAGS=$HDF5_LINK_OPTS
+export CPPFLAGS="$HDF5_INCLUDE_OPTS -I$NETCDF/include"
+export LDFLAGS="$HDF5_LINK_OPTS -L$NETCDF/lib"
 
 ###########################################################
 cd ../../
 lwd=`pwd`
 cd $cwd
 LOCAL_EXTERN=${lwd}/ext_libs/${LIBNAME}_${VER}
-mkdir -p $LOCAL_EXTERN
-rm -rf $LOCAL_EXTERN/*
+if [ ! -d $LOCAL_EXTERN ]; then echo "Build first netcdf_v4.4.1.1!" ; exit 9 ; fi
 ###########################################################
-tar xf netcdf-c-4.4.1.1.tar.gz
-cd netcdf-c-4.4.1.1
+tar xf netcdf-fortran-4.4.4.tar.gz
+cd netcdf-fortran-4.4.4
+#####
+cp ../configure .
+#####
 ###########################################################
-./configure --prefix=$LOCAL_EXTERN  --disable-shared --disable-dap
+./configure --prefix=$LOCAL_EXTERN  --disable-shared
 make all
 make install
 cd $cwd
-rm -rf netcdf-c-4.4.1.1
+rm -rf netcdf-fortran-4.4.4
 ###########################################################
-
-#
-#     Create modulefile
-#
-cd ../../
-lwd=`pwd`
-cd $cwd
-mkdir -p $lwd/modulefiles/$LIBNAME
-cat modulefile.template | sed s:_CWD_:$lwd:g > $lwd/modulefiles/$LIBNAME/$VER
 
 exit
